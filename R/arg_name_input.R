324c1bcf198cc44fb7eea47006308559fdc43cee
Josh Livingston <joshwlivingston@gmail.com>
2025-12-29 10:47:30 -0700

    style: move `arg_name_input_*()` functions to same file

--- R/arg_name_input.R
+++ R/arg_name_input.R
@@ -1,3 +1,53 @@
+# Generic: arg_name_input_id ####
+arg_name_input_id <- new_generic(
+	name = "arg_name_input_id",
+	dispatch_args = c("x")
+)
+
+## Method: character | Date | factor | logical | list | numeric | POSIXt ####
+method(
+	arg_name_input_id,
+	class_character |
+		class_Date |
+		class_factor |
+		class_logical |
+		class_list |
+		class_numeric |
+		class_POSIXt
+) <- function(x, ...) {
+	"inputId"
+}
+
+## Method: data.frame ####
+method(arg_name_input_id, class_data.frame) <- function(x, ...) {
+	stop("`arg_name_input_id()` is not implemented for data.frames.")
+}
+
+# Generic: arg_name_input_label ####
+arg_name_input_label <- new_generic(
+	name = "arg_name_input_label",
+	dispatch_args = c("x")
+)
+
+## Method: character | Date | factor | logical | list | numeric | POSIXt ####
+method(
+	arg_name_input_label,
+	class_character |
+		class_Date |
+		class_factor |
+		class_logical |
+		class_list |
+		class_numeric |
+		class_POSIXt
+) <- function(x, ...) {
+	"label"
+}
+
+## Method: data.frame ####
+method(arg_name_input_label, class_data.frame) <- function(x, ...) {
+	stop("`arg_name_input_label()` is not implemented for data.frames.")
+}
+
 # Generic: arg_name_input_value ####
 arg_name_input_value <- new_generic(
 	name = "arg_name_input_value",
