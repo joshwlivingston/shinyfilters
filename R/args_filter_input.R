4e4ea3937806905d07624da8b8a5647ce7e3bf58
Josh Livingston <joshwlivingston@gmail.com>
2026-01-02 09:28:30 -0700

    fix: ensure necessary arguments removed from args_update_filter_input

--- R/args_filter_input.R
+++ R/args_filter_input.R
@@ -160,7 +160,10 @@ args_update_filter_input <- function(x, ...) {
 		args[[arg_name_input_id(x)]] <- NULL
 	}
 	if (all(arg_name_input_value(x) %in% names(args))) {
-		args[[arg_name_input_value(x)]] <- NULL
+		args <- args[names(args) != arg_name_input_value(x)]
+		if (identical(length(args), 0L)) {
+			return(NULL)
+		}
 	}
 	return(args)
 }
